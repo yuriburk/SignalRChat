62ffee0e2db038f9b074a4578c7e6f7116bf220c
Jefferson Yuri Cunha Burk <jefferson.burk@hbsis.com.br>
2019-11-17 20:13:33 -0300

    Criado projeto de front

---
+++ client/team-annotations/src/App.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import { HubConnection } from 'signalr-client-react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('Anônimo');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [hubConnection, setHubConnection] = useState(null);
+
+  useEffect(() => {
+    setName(window.prompt('Seu nome: ', 'Yuri'));
+    setHubConnection(new HubConnection('http://localhost:61927/chathub'));
+  }, []);
+
+  useEffect(() => {
+    if (hubConnection) {
+      hubConnection.on('sendToAll', (nick, receivedMessage) => {
+        const text = `${nick}: ${receivedMessage}`;
+        setMessages(messages.concat([text]));
+      });
+
+      hubConnection.start()
+                   .then(() => console.log('Connection started!'))
+                   .catch(err => console.log('Error while establishing connection :('));
+    }
+  }, [hubConnection]);
+
+  function sendMessage() {
+    hubConnection
+      .invoke('sendToAll', name, message)
+      .catch(err => console.error(err));
+  
+      setMessage('');      
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <br />
+            <input
+             type="text"
+             value={message}
+             onChange={e => setMessage(e.target.value)}
+            />
+
+            <button onClick={() => sendMessage()}>Send</button>
+
+            <div>
+              {messages.map((message, index) => (
+                <span style={{display: 'block'}} key={index}> {message} </span>
+              ))}
+            </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

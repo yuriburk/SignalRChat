491d26c0086f3288507c6f4e053a521fe5dec3b1
Jefferson Yuri Cunha Burk <jefferson.burk@hbsis.com.br>
2019-11-18 16:09:22 -0300

    Criado estilização para o chat

--- client/team-annotations/src/components/Chat/index.js
+++ client/team-annotations/src/components/Chat/index.js
@@ -1,29 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import * as signalR from '@aspnet/signalr';
+import React, { useState, useEffect } from "react";
+import * as signalR from "@aspnet/signalr";
+import { Container, MessagesBox } from "./styles";
 
 function Chat() {
-  const [name, setName] = useState('Anônimo');
-  const [message, setMessage] = useState('');
+  const ENTER_KEY_CODE = 13; 
+  const [name, setName] = useState("Anônimo");
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [hubConnection, setHubConnection] = useState(null);
 
   useEffect(() => {
-    setName(window.prompt('Seu nome: ', 'Yuri'));
-    setHubConnection(new signalR.HubConnectionBuilder().withUrl('http://192.168.17.26/TeamAnnotations/chathub').build());
+    setName(window.prompt("Seu nome: ", "Yuri"));
+    setHubConnection(
+      new signalR.HubConnectionBuilder()
+        .withUrl("http://192.168.17.26/TeamAnnotations/chathub")
+        .build()
+    );
   }, []);
 
   useEffect(() => {
     if (hubConnection) {
-      hubConnection.on('sendMessage', (annotation) => {
+      hubConnection.on("sendMessage", annotation => {
         const text = `${annotation.name}: ${annotation.text}`;
         setMessages(oldMessages => [...oldMessages, text]);
       });
 
-      hubConnection.start()
-                   .then(() => console.log('Connection started!'))
-                   .catch(err => console.log('Error while establishing connection :('));
+      hubConnection
+        .start()
+        .then(() => console.log("Connection started!"))
+        .catch(err => console.log("Error while establishing connection :("));
     }
   }, [hubConnection]);
+
+  function handleKeyUp(event) {
+    var code = event.keyCode || event.which;
+    if (code === ENTER_KEY_CODE && message
+      ) {
+      sendMessage();
+    }
+  }
 
   function sendMessage() {
     var annotation = {
@@ -31,33 +46,35 @@ function Chat() {
       Text: message
     };
     hubConnection
-      .invoke('SendAnnotation', annotation)
+      .invoke("SendAnnotation", annotation)
       .catch(err => console.error(err));
-  
-      setMessage('');      
-  };
+    setMessage("");
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <br />
-            <input
-             type="text"
-             value={message}
-             onChange={e => setMessage(e.target.value)}
-            />
+    <Container>
+      {messages.map((messageReceived, index) => (
+        <span style={{ display: "block" }} key={index}>
+          {" "}
+          {messageReceived}{" "}
+        </span>
+      ))}
 
-            <button onClick={() => sendMessage()}>Send</button>
-
-            <div>
-              {messages.map((messageReceived, index) => (
-                <span style={{display: 'block'}} key={index}> {messageReceived} </span>
-              ))}
-            </div>
-        </div>
-      </header>
-    </div>
+      <MessagesBox>
+        <input
+          type="text"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          onKeyPress={handleKeyUp}
+          style={{ width: "100%" }}
+        />
+        <button
+          onClick={() => sendMessage()}
+        >
+          Send
+        </button>
+      </MessagesBox>
+    </Container>
   );
 }
 
---
+++ client/team-annotations/src/components/Chat/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+`;
+
+export const MessagesBox = styled.div`
+    display: flex;
+    align-items: stretch;
+`;
